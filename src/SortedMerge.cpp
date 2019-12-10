65e84b08fea523c62d0c17962620a4726cfbd0c5
Nguyen Bui <n4bui@ucsd.edu>
2019-12-10 01:44:30 -0800

    add test cases

--- src/SortedMerge.cpp
+++ src/SortedMerge.cpp
@@ -1,17 +1,15 @@
 #include "SortedMerge.hpp"
+#include <iostream>
 
 void SortedMerge::solution(std::vector<int>& l1, std::vector<int>& l2)
 {
 	int l1_ind = l1.size() - 1;
 	int l2_ind = l2.size() - 1;
 	int merg_ind = l1.size() + l2.size() - 1;
-	if (l1_ind < 0) {
-		return;
-	}
 
 	l2.resize(int(merg_ind+1));
 
-	while (merg_ind>=0 && l1_ind>=0){
+	while (l1_ind>=0 && l2_ind>=0){
 		if ( l1[l1_ind] > l2[l2_ind]) {
 			l2[merg_ind] = l1[l1_ind];
 			--l1_ind;
@@ -21,5 +19,12 @@ void SortedMerge::solution(std::vector<int>& l1, std::vector<int>& l2)
 			--l2_ind;
 		}
 		--merg_ind;
+	}
+
+	if (l2_ind < 0 && l1_ind >= 0) {
+		while (l1_ind >= 0) {
+			l2[l1_ind] = l1[l1_ind];
+			--l1_ind;
+		}
 	}
 }
--- test/AllTests.cpp
+++ test/AllTests.cpp
@@ -15,7 +15,53 @@ TEST(TEST_SORTED_MERGE, NONE_EMPTY_SORTED_MERGE) {
 	SortedMerge sortedMerge;
 	sortedMerge.solution(l1, l2);
 
-	for (int i = 0; i < res.size();i++) {
+	for (int i = 0; i < res.size();++i) {
 		EXPECT_EQ(res[i], l2[i]);
+	}
+}
+
+TEST(TEST_SORTED_MERGE, FIRST_ARRAY_EMPTY_SORTED_MERGE) {
+	std::vector<int> l1 = {};
+	std::vector<int> l2 = { 0,2,4,6,8 };
+	std::vector<int> res = { 0,2,4,6,8 };
+	SortedMerge sortedMerge;
+	sortedMerge.solution(l1, l2);
+
+	for (int i = 0; i < res.size();++i) {
+		EXPECT_EQ(res[i], l2[i]);
+	}
+}
+
+TEST(TEST_SORTED_MERGE, SECOND_ARRAY_EMPTY_SORTED_MERGE) {
+	std::vector<int> l1 = { 1,3,5,7,9 };
+	std::vector<int> l2 = {};
+	std::vector<int> res = { 1,3,5,7,9};
+	SortedMerge sortedMerge;
+	sortedMerge.solution(l1, l2);
+
+	for (int i = 0; i < res.size();++i) {
+		EXPECT_EQ(res[i], l2[i]);
+	}
+}
+
+TEST(TEST_SORTED_MERGE, BOTH_ARRAYS_EMPTY_SORTED_MERGE) {
+	std::vector<int> l1 = {};
+	std::vector<int> l2 = {};
+
+	SortedMerge sortedMerge;
+	sortedMerge.solution(l1, l2);
+
+	EXPECT_EQ(l2.size(), 0);
+}
+
+TEST(TEST_SORTED_MERGE, NEGATIVE_ARRAYS_SORTED_MERGE) {
+	std::vector<int> l1{ -5,-3,-1,1,3,5 };
+	std::vector<int> l2{ -4,-2,0,2,4 };
+	std::vector<int> res{ -5,-4,-3,-2,-1,0,1,2,3,4,5 };
+	SortedMerge sortedMerge;
+	sortedMerge.solution(l1, l2);
+
+	for (int i = 0; i < res.size();++i) {
+		EXPECT_EQ(res[i], res[i]);
 	}
 }
