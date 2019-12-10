e1f9386f16060d3f97b8c39cac9c3500a8a13050
Nguyen Bui <n4bui@ucsd.edu>
2019-12-10 01:49:09 -0800

    Add docs

--- src/SortedMerge.cpp
+++ src/SortedMerge.cpp
@@ -1,6 +1,12 @@
 #include "SortedMerge.hpp"
 #include <iostream>
 
+/*
+ * input: l1, l2: array of ascending-sorted ints
+ * output: void
+ * usage: Merge 2 array of ascending-sorted ints into the second array
+ *        Second array will be resize to fit the sum of inputs
+ */
 void SortedMerge::solution(std::vector<int>& l1, std::vector<int>& l2)
 {
 	int l1_ind = l1.size() - 1;
@@ -9,6 +15,7 @@ void SortedMerge::solution(std::vector<int>& l1, std::vector<int>& l2)
 
 	l2.resize(int(merg_ind+1));
 
+	// Merge 2 list until 1 empty
 	while (l1_ind>=0 && l2_ind>=0){
 		if ( l1[l1_ind] > l2[l2_ind]) {
 			l2[merg_ind] = l1[l1_ind];
@@ -21,6 +28,7 @@ void SortedMerge::solution(std::vector<int>& l1, std::vector<int>& l2)
 		--merg_ind;
 	}
 
+	// Copy over the remaining l1
 	if (l2_ind < 0 && l1_ind >= 0) {
 		while (l1_ind >= 0) {
 			l2[l1_ind] = l1[l1_ind];
